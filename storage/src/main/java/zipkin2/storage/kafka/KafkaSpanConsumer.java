2bb52a5d10ce72297ec11d3bdd0765680209f8fb
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2019-02-05 21:51:35 +0100

    feat: initial span consumer testing
    Using testcontainers to test KafkaSpanConsumer plus adding headers.

--- storage/src/main/java/zipkin2/storage/kafka/KafkaSpanConsumer.java
+++ storage/src/main/java/zipkin2/storage/kafka/KafkaSpanConsumer.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2019 [name of copyright owner]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package zipkin2.storage.kafka;
 
 import org.apache.kafka.clients.producer.Producer;
@@ -14,6 +27,7 @@ import java.util.List;
 
 public class KafkaSpanConsumer implements SpanConsumer {
 
+    static final String TOPIC = "zipkin-spans-proto3_v2";
     final Producer<String, byte[]> producer;
 
     KafkaSpanConsumer(KafkaStorage kafkaStorage) {
@@ -38,7 +52,7 @@ public class KafkaSpanConsumer implements SpanConsumer {
         static Call<Void> create(Producer<String, byte[]> producer, Span span) {
             byte[] encodedSpan = SpanBytesEncoder.PROTO3.encode(span);
             StoreSpanProto3Call call = new StoreSpanProto3Call(
-                    producer, "zipkin-spans-proto3_v2", span.traceId(), encodedSpan);
+                    producer, TOPIC, span.traceId(), encodedSpan);
             return call.handleError(call);
         }
 
---
+++ storage/src/test/java/zipkin2/storage/kafka/KafkaStorageIT.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 [name of copyright owner]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package zipkin2.storage.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.KafkaContainer;
+import zipkin2.Callback;
+import zipkin2.Span;
+import zipkin2.storage.StorageComponent;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+import static zipkin2.TestObjects.TODAY;
+
+public class KafkaStorageIT {
+    @Rule
+    public KafkaContainer kafka = new KafkaContainer("4.1.2");
+
+    @Test
+    public void should_consume_spans() {
+        StorageComponent storage = new KafkaStorage.Builder().bootstrapServers(kafka.getBootstrapServers()).build();
+        Span root = Span.newBuilder().traceId("a").id("a").timestamp(TODAY).duration(10).build();
+        storage.spanConsumer().accept(Collections.singletonList(root)).enqueue(new Callback<Void>() {
+            @Override
+            public void onSuccess(Void value) {
+                assertTrue(true);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                fail("Error consuming span");
+            }
+        });
+
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OffsetResetStrategy.EARLIEST.name().toLowerCase());
+        KafkaConsumer<String, byte[]> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Collections.singletonList(KafkaSpanConsumer.TOPIC));
+        ConsumerRecords<String, byte[]> records = kafkaConsumer.poll(Duration.ofSeconds(5));
+        assertEquals(1, records.count());
+    }
+
+}
