2ddcbaafad35d516e02c9db335061664b6960266
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2019-02-06 16:37:06 +0100

    feat: initial draft auto configure

---
+++ autoconfigure/src/main/java/zipkin2/autoconfigure/storage/kafka/ZipkinKafkaStorageProperties.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 [name of copyright owner]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package zipkin2.autoconfigure.storage.kafka;
+
+import zipkin2.storage.kafka.KafkaStorage;
+
+import java.io.Serializable;
+
+public class ZipkinKafkaStorageProperties implements Serializable {
+    private static final long serialVersionUID = 0L;
+
+    private String bootstrapServers = "localhost";
+
+    public KafkaStorage.Builder toBuilder() {
+        return KafkaStorage.newBuilder().bootstrapServers(bootstrapServers);
+    }
+
+    public String getBootstrapServers() {
+        return bootstrapServers;
+    }
+
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+}
