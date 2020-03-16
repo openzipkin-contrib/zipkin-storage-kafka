e84129561c96e19dee3320e2d7a4cb6fef45e960
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-03-16 12:44:32 +0100

    chore: bump versions

--- storage/src/main/java/zipkin2/storage/kafka/KafkaSpanStore.java
+++ storage/src/main/java/zipkin2/storage/kafka/KafkaSpanStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 The OpenZipkin Authors
+ * Copyright 2019-2020 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -80,7 +80,6 @@ final class KafkaSpanStore implements SpanStore, Traces, ServiceAndSpanNames {
     }
   }
 
-  @SuppressWarnings("deprecation")
   @Override public Call<List<Span>> getTrace(String traceId) {
     if (traceByIdQueryEnabled) {
       return new GetTraceCall(storage.getTraceStorageStream(), httpBaseUrl,
